421db0aadab6c20a5c6c1e25b6bd434cc3dd93a4
hotao-gbp <gkxogh02@naver.com>
2025-08-28 19:22:35 +0900

    save

--- app/simulation/routes.py
+++ app/simulation/routes.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template, jsonify
+import requests
 import random
+from flask import Blueprint, render_template, jsonify, request
+
+
 
 simulation_bp = Blueprint('simulation', __name__, url_prefix='/simulation')
 
@@ -19,3 +22,14 @@ def simulation():
 def simulation_data():
     data = generate_wind_data()
     return jsonify(data)
+
+RASPBERRY_PI_URL = "http://192.168.0.6:5001/set_environment"
+
+@simulation_bp.route('/send_settings', methods=['POST'])
+def send_settings():
+    data = request.json  # {'direction':..., 'speed':..., ...}
+    try:
+        resp = requests.post(RASPBERRY_PI_URL, json=data, timeout=3)
+        return jsonify({'status': 'ok', 'rpi_response': resp.json()})
+    except Exception as e:
+        return jsonify({'status': 'fail', 'error': str(e)})
